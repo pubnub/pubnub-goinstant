980d9e6027634d05b6ea152ead4de91b70cc05c6
Jasdeep Jaitla <jasdeep@scalabl3.com>
2014-08-04 10:12:34 -0700

    update compiled js

--- js/pubnub-goinstant.js
+++ js/pubnub-goinstant.js
@@ -1,3 +1,19 @@
+var hasValue = function(v) {
+    return (!!(typeof v !== 'undefined' && v !== null));
+};
+
+var isFunction = function(fn) {
+    return (!!(typeof fn !== 'undefined' && fn !== null && typeof fn === 'function'));
+};
+
+var isObject = function(v) {
+    return (!!(typeof v === 'object' && v !== null));
+};
+
+/* **********************************************
+     Begin stampit_0.7.1.js
+********************************************** */
+
 !function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.stampit=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 var forIn = _dereq_('mout/object/forIn');
 
@@ -2626,22 +2642,6 @@ return Q;
 
 });
 
-
-/* **********************************************
-     Begin utility.js
-********************************************** */
-
-var hasValue = function(v) {
-    return (!!(typeof v !== 'undefined' && v !== null));
-};
-
-var isFunction = function(fn) {
-    return (!!(typeof fn !== 'undefined' && fn !== null && typeof fn === 'function'));
-};
-
-var isObject = function(v) {
-    return (!!(typeof v === 'object' && v !== null));
-};
 
 /* **********************************************
      Begin goinstant2.js
